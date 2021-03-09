cbdfb7008040aee547e14bf95d3bcb97e285a622
Jonatan Tomás Caro <al375877@uji.es>
2021-03-09 15:10:09 +0100

    Creando Etapas, falta ex, mem y wb

--- practica1/practica1.py
+++ practica1/practica1.py
@@ -33,15 +33,15 @@ class Instruccion:
         return self.id_rt
 
     def getInm(self):
-        return self.inm
+        return int(self.inm)
 
     def toSring(self):
         if self.operacion=="NOP":
             return "NOP"
         if self.operacion=="sw":
-            return self.operacion + " " + self.id_rs + ", " + self.id_rt
+            return self.operacion + " " + self.id_rt + ", " + self.inm + "(" + self.id_rs + ")"
         if self.operacion=="lw":
-            return self.operacion + " " + self.id_rd + ", " + self.id_rs
+            return self.operacion + " " + self.id_rt + ", " + self.inm + "(" + self.id_rs + ")"
         return self.operacion + " " + self.id_rd + ", " + self.id_rs + ", " + self.id_rt
 
 class Reg_IF_ID:
@@ -52,15 +52,6 @@ class Reg_IF_ID:
     id_rt = None
     id_rd = None
     inmediato = None
-
-    def __init__(self, instruccion, tipo, operacion, id_rs, id_rt, id_rd, inm):
-        self.instruccion = instruccion
-        self.tipo = tipo
-        self.operacion = operacion
-        self.id_rs = id_rs
-        self.id_rt = id_rt
-        self.id_rd = id_rd
-        self.inm = inm
 
     def getInstruccion(self):
         return self.instruccion
@@ -114,17 +105,6 @@ class Reg_ID_EX:
     inmediato = None
     op1 = None
     op2 = None
-
-    def __init__(self, instruccion, tipo, operacion, id_rs, id_rt, id_rd, inm, op1, op2):
-        self.instruccion = instruccion
-        self.tipo = tipo
-        self.operacion = operacion
-        self.id_rs = id_rs
-        self.id_rt = id_rt
-        self.id_rd = id_rd
-        self.inm = inm
-        self.op1 = op1
-        self.op2 = op2
 
     def getInstruccion(self):
         return self.instruccion
@@ -280,13 +260,67 @@ class Reg_MEM_WB:
 # ******************************************************************************************************
 #    SIMULADOR
 # ******************************************************************************************************
-def ejecutaInstruccion(self, instruccion):
-    registros = {'r0': None, 'r1': None,'r2': None,'r3': None,'r4': None,'r5': None,'r6': None,
-                 'r7': None,'r8': None,'r9': None,'r10': None,'r11': None,'r12': None,
-                 'r13': None,'r14': None,'r15': None,}
-    operaciones = ["lw", "sw", "add", "sub"]
+def ejecutaInstruccion(instruccion, registros):
+    registrosNuevos = registros
+    if instruccion.getOperacion() == "lw" or instruccion.getOperacion() == "sw":
+        registrosNuevos[instruccion.getRt()]=registros.get(instruccion.getRs())+instruccion.getInm()
+    if instruccion.getOperacion() == "add":
+        registrosNuevos[instruccion.getRd()]=registros.get(instruccion.getRs())+registros.get(instruccion.getRt())
+    if instruccion.getOperacion() == "sub":
+        registrosNuevos[instruccion.getRd()]=registros.get(instruccion.getRs())-registros.get(instruccion.getRt())
 
+    return registrosNuevos
 
+def ejecutaEtapa(etapa, instruccion, registros, numInst ):
+
+    if etapa == "IF":
+        registro = Reg_IF_ID()
+        registro.setInstruccion(instruccion)
+        registro.setRs(instruccion.getRs())
+        registro.setRt(instruccion.getRt())
+        registro.setRd(instruccion.getRd())
+        registro.setInm(instruccion.getInm())
+        registro.setOperacion(instruccion.getOperacion())
+        if instruccion.getOperacion() == "sw" or instruccion.getOperacion() == "lw":
+            registro.setTipo("MEM")
+        else:
+            registro.setTipo("ALU")
+        registrosNuevos = ejecutaInstruccion(instruccion, registros)
+        print("  Etapa IF de I" + numInst +": Mostrar contenido del registro RS_IF_ID")
+        print("    Instrucción: " + registro.getInstruccion().toSring())
+        print("    Tipo: " + registro.getTipo())
+        print("    Operación: " + registro.getOperacion())
+        print("    Rs: " + registro.getRs() + ", Rt: " + registro.getRt() +
+              ", Rd: " + registro.getRd() + ", Inm: " + str(registro.getInm()))
+        return registrosNuevos
+
+    if etapa == "ID/OF":
+        registro = Reg_ID_EX()
+        registro.setInstruccion(instruccion)
+        registro.setRs(instruccion.getRs())
+        registro.setRt(instruccion.getRt())
+        registro.setRd(instruccion.getRd())
+        registro.setInm(instruccion.getInm())
+        registro.setOp1(registros[instruccion.getRs()])
+        registro.setOp2(registros[instruccion.getRt()])
+        registro.setOperacion(instruccion.getOperacion())
+        if instruccion.getOperacion() == "sw" or instruccion.getOperacion() == "lw":
+            registro.setTipo("MEM")
+        else:
+            registro.setTipo("ALU")
+        registrosNuevos = ejecutaInstruccion(instruccion, registros)
+        print("  Etapa ID/OF de I" + numInst +": Mostrar contenido del registro RS_ID_EX")
+        print("    Instrucción: " + registro.getInstruccion().toSring())
+        print("    Tipo: " + registro.getTipo())
+        print("    Operación: " + registro.getOperacion())
+        if registro.getTipo() == "ALU":
+            print("    Rs: " + registro.getRs() + ", Rt: " + registro.getRt() + ", Rd: " + registro.getRd() +
+                  ", Op1: " + str(registro.getOp1()) + ", Op2: " + str(registro.getOp2()))
+        else:
+            print("    Rt: " + registro.getRt() + ", Rs: " + registro.getRs() + ", Inm: " + str(registro.getInm()))
+        return registrosNuevos
+
+    #TODO FALTA ETAPA EX, MEM Y WB
 
 # ******************************************************************************************************
 #    METODO DE ENTRADA Y SALIDA
@@ -302,19 +336,21 @@ def crearInstrucciones(entrada):
 
     for linea in entrada:
         elem = linea.rstrip("\n").split(" ")
-        if elem[0] == "lw":
-            inst = Instruccion(operacion=elem[0], id_rd=elem[1], id_rs=elem[2], inm=elem[2], id_rt=" ")
-        elif elem[0] == "sw":
-            inst = Instruccion(operacion=elem[0], id_rt=elem[2], id_rs=elem[1], inm=elem[2], id_rd=" ")
+        if elem[0] == "lw" or elem[0] == "sw":
+            elem2=elem[2].split("(")
+            inm=elem2[0]
+            elem3=elem2[1].split(")")
+            rs=elem3[0]
+            inst = Instruccion(operacion=elem[0], id_rt=elem[1], id_rs=rs, inm=inm, id_rd=" ")
         else:
-            inst = Instruccion(operacion=elem[0], id_rd=elem[1], id_rs=elem[2], id_rt=elem[3], inm=" ")
+            inst = Instruccion(operacion=elem[0], id_rd=elem[1], id_rs=elem[2], id_rt=elem[3], inm=0)
 
         if len(instrucciones) != 0:
             instAnt = instrucciones[-1]
             if instAnt.getOperacion() == "lw":
                 if inst.getOperacion() == "add" or inst.getOperacion() == "sub":
-                    if inst.getRt() == instAnt.getRd() or inst.getRs() == instAnt.getRd():
-                        instrucciones.append(Instruccion(operacion="NOP", id_rd=" ", id_rs=" ", id_rt=" ", inm=" "))
+                    if inst.getRt() == instAnt.getRt() or inst.getRs() == instAnt.getRt():
+                        instrucciones.append(Instruccion(operacion="NOP", id_rd="0", id_rs="0", id_rt="0", inm=0))
 
         instrucciones.append(inst)
 
@@ -328,6 +364,9 @@ if __name__ == '__main__':
 
     entrada = [a for a in leerFichero()]
     instrucciones= crearInstrucciones(entrada)
+    registros = {'r0': 0, 'r1': 1,'r2': 2,'r3': 3,'r4': 4,'r5': 5,'r6': 6,
+                 'r7': 7,'r8': 8,'r9': 9,'r10': 10,'r11': 11,'r12': 12,
+                 'r13': 13,'r14': 14,'r15': 15}
 
     print(" ")
     print("Sea un programa formado por las siguientes instrucciones: ")
@@ -336,6 +375,14 @@ if __name__ == '__main__':
     print(" ")
     print("Simulación ejecución: ")
     i=1
+    # for elem in instrucciones:
     print("  Ciclo " + str(i) + ":")
+    registros = ejecutaEtapa("IF",instrucciones[0],registros,str(i))
+    registros = ejecutaEtapa("ID/OF", instrucciones[0], registros, str(i))
+        # i=i+1
+
+
+
+
 
 
